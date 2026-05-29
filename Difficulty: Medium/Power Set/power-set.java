19896da91086de11d18d26858a7287e7c5650b55
Pavan Kumar <159564756+Student-Pavan@users.noreply.github.com>
2026-05-29 14:11:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Power Set/power-set.java
+++ Difficulty: Medium/Power Set/power-set.java
@@ -3,26 +3,21 @@
 class Solution {
 	public List<String> AllPossibleStrings(String s) {
 		// Code here
-		List<String> a = new ArrayList<>();
-		
-		subsequences(s, 0, "", a);
-		
-		a.remove("");
-		
-		Collections.sort(a);
-		
-		return a;
+		List<String> result = new ArrayList<>();
+        int n = s.length();
+        for (int i = 0; i < (1 << n); i++) {
+            StringBuilder subset = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    subset.append(s.charAt(j));
+                }
+            }
+            if (subset.length() > 0) {
+                result.add(subset.toString());
+            }
+        }
+        Collections.sort(result);
+        return result;
 	}
 	
-	private void subsequences(String s, int idx, String newstr, List<String> a) {
-		if (s.length() == idx) {
-			a.add(newstr);
-			return ;
-		}
-		
-		char currchar = s.charAt(idx);
-		subsequences(s, idx + 1, newstr + currchar , a);
-		subsequences(s, idx + 1, newstr, a);
-		
-	}
 }
