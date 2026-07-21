b4627793f93def96c3eaccdc5f848364f3b7bb9e
Pavan Kumar <159564756+Student-Pavan@users.noreply.github.com>
2026-07-21 20:52:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Reverse a sublist of a linked list/reverse-a-sublist-of-a-linked-list.java
+++ Difficulty: Hard/Reverse a sublist of a linked list/reverse-a-sublist-of-a-linked-list.java
@@ -1,66 +1,44 @@
 /* Structure of a Linked List Node
 class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
+	int data;
+	Node next;
+	
+	Node(int x) {
+		data = x;
+		next = null;
+	}
 }
 */
+
 class Solution {
-    Node reverseBetween(int a, int b, Node head) {
-
-        if (head == null || a == b)
-            return head;
-
-        Node first = head;
-        int len = 1;
-
-        // Find first node
-        while (len < a) {
-            first = first.next;
-            len++;
-        }
-
-        Node beforefirst = null;
-        if (a != 1) {
-            beforefirst = head;
-            while (beforefirst.next != first) {
-                beforefirst = beforefirst.next;
-            }
-        }
-
-        Node second = head;
-        int len1 = 1;
-
-        // Find second node
-        while (len1 < b) {
-            second = second.next;
-            len1++;
-        }
-
-        Node aftersecond = second.next;
-
-        // Reverse from first to second
-        Node prev = aftersecond;
-        Node temp = first;
-        Node next = null;
-
-        while (temp != aftersecond) {
-            next = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = next;
-        }
-
-        // Reconnect
-        if (beforefirst != null)
-            beforefirst.next = prev;
-        else
-            head = prev;
-
-        return head;
-    }
+	Node reverseBetween(int a, int b, Node head) {
+		// code here
+		Node dummy = new Node(0);
+		dummy.next = head;
+		
+		Node prevleft = dummy;
+		
+		for (int i = 1; i < a; i++)
+			prevleft = prevleft.next;
+		
+		Node curr = prevleft.next;
+		
+		Node prev = null;
+		
+		
+		for (int i = a; i<= b ; i++){
+		    Node next = curr.next;
+		    curr.next = prev;
+            prev = curr;
+            curr = next;
+		}
+		
+		Node left = prevleft.next;
+		prevleft.next = prev;
+		left.next = curr;
+		
+		
+		return dummy.next;
+	}
+		
 }
