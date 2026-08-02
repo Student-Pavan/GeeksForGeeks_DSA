de4efb4e08cfbe39438e98a84b40dba98e3dd8cf
Pavan Kumar <159564756+Student-Pavan@users.noreply.github.com>
2026-08-02 16:47:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Mirror Tree/mirror-tree.java
@@ -0,0 +1,39 @@
+/* Structure of Binary Tree Node
+class Node
+{
+	int data;
+	Node left, right;
+	Node(int item)
+	{
+		data = item;
+		left = right = null;
+	}
+} */
+
+class Solution {
+	void mirror(Node root) {
+		// code here
+		Queue<Node> q = new LinkedList<>();
+		
+		q.offer(root);
+		
+		while (!q.isEmpty()) {
+			
+			Node curr = q.poll();
+			
+			Node temp = curr.left;
+			
+			curr.left = curr.right;
+			curr.right = temp;
+			
+			if (curr.left != null) {
+				q.offer(curr.left);
+			}
+			if (curr.right != null) {
+				q.offer(curr.right);
+			}
+			
+		}
+		
+	}
+}
