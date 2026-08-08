5e95ec05196189c8980659b2a07ee8b31d155883
Pavan Kumar <159564756+Student-Pavan@users.noreply.github.com>
2026-08-08 19:05:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BST Insert/bst-insert.java
@@ -0,0 +1,30 @@
+/* Structure of a Binary Search Tree node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    public Node insert(Node root, int key) {
+        // code here
+        if(root == null){
+            return new Node(key);
+        }
+        
+        if(key < root.data){
+            root.left =  insert(root.left, key);
+        }
+        
+        else if(key > root.data){
+            root.right =  insert(root.right, key);
+        }
+        
+        return root;
+        
+    }
+}
