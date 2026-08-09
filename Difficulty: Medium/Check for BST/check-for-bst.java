8c2f0801ef045fddd956c679127c4db53f06ad33
Pavan Kumar <159564756+Student-Pavan@users.noreply.github.com>
2026-08-09 16:51:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Check for BST/check-for-bst.java
+++ Difficulty: Medium/Check for BST/check-for-bst.java
@@ -1,27 +1,30 @@
-/*
+/* Structure of a Binary Search Tree node
 class Node {
-	int data;
-	Node left;
-	Node right;
-	
-	Node(int val) {
-		data = val;
-		left = right = null;
-	}
-}
-*/
+    int data;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+} */
 
 class Solution {
-	public boolean isBST(Node root) {
-		// code here
-		return checkBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-	}
-	private boolean checkBST(Node root, long min, long max) {
-		if (root == null)
-			return true;
-		if (root.data <= min || root.data >= max)
-			return false;
-		
-		return checkBST(root.left, min, root.data) && checkBST(root.right, root.data, max);
-	}
+    public boolean isBST(Node root) {
+        // code herereturn 
+        return checkBST(root, Long.MIN_VALUE ,Long.MAX_VALUE);
+    }
+    
+    public boolean checkBST(Node root, long min, long max){
+        if(root == null)
+            return true;
+            
+        if(root.data <= min || root.data >= max)
+            return false;
+            
+        return checkBST(root.left, min,root.data) &&
+               checkBST(root.right, root.data, max);
+    }
+    
 }
