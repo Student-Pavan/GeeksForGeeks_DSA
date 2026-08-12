2cec062a98dcce3a430c5a800bd8bf68b16e48d0
Pavan Kumar <159564756+Student-Pavan@users.noreply.github.com>
2026-08-12 18:16:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
@@ -14,10 +14,38 @@ class Solution {
     public ArrayList<Integer> merge(Node r1, Node r2) {
         // code here
         ArrayList<Integer> list = new ArrayList<>();
-        inorder(r1,list);
-        inorder(r2,list);
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        inorder(r1,list1);
+        inorder(r2,list2);
         
-        Collections.sort(list);
+        int i = 0, j=0;
+        
+        while(i < list1.size() && j < list2.size()){
+            int a = list1.get(i);
+            int b = list2.get(j);
+            if( a < b){
+                list.add(a);
+                i++;
+            }
+            else{
+                list.add(b);
+                j++;
+                
+            }
+                
+            
+        }
+        
+        while(i < list1.size()){
+            list.add(list1.get(i));
+            i++;
+        }
+        while(j < list2.size()){
+            list.add(list2.get(j));
+            j++;
+        }
+        
         return list;
     }
     
